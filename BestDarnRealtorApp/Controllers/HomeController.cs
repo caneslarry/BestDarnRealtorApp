63281cd8926a9a5f749fca748849621ed3f1a15d
larry <caneslarry@gmail.com>
2021-02-25 17:39:42 -0500

    - inital

---
+++ BestDarnRealtorApp/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BestDarnRealtorApp.Models;
+using BestDarnRealtorApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestDarnRealtorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BestDarnRealtorAppContext _context;
+
+        public HomeController(ILogger<HomeController> logger, BestDarnRealtorAppContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View(new SearchForm());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchForm model)
+        {
+            IQueryable<Listing> query = _context.Listing;
+
+            query = this.buildContextQuery(model, query);
+
+            await query.ToListAsync();
+
+         
+            //var model = new SearchForm();
+            model.Listings = query;
+
+            return View(model);
+        }
+
+        private IQueryable<Listing> buildContextQuery(SearchForm form, IQueryable<Listing> query)
+        {
+            if (form.mls != null)
+            {
+                query = query.Where(l => l.Mls == form.mls);
+            }
+            if (form.city != null)
+            {
+                query = query.Where(l => l.City == form.city);
+            }
+            if (form.state != null)
+            {
+                query = query.Where(l => l.State == form.state);
+            }
+            if (form.zip != null)
+            {
+                query = query.Where(l => l.Zip == form.zip);
+            }
+            if (form.bedrooms_min > 0)
+            {
+                query = query.Where(l => l.Bedrooms >= form.bedrooms_min);
+            }
+            if (form.bathrooms_min > 0)
+            {
+                query = query.Where(l => l.Bathrooms >= form.bathrooms_min);
+            }
+            if (form.squarefeet_min > 0)
+            {
+                query = query.Where(l => l.SquareFeet >= form.squarefeet_min);
+            }
+
+            if (form.bedrooms_max > 0)
+            {
+                query = query.Where(l => l.Bedrooms <= form.bedrooms_max);
+            }
+            if (form.bathrooms_max > 0)
+            {
+                query = query.Where(l => l.Bathrooms <= form.bathrooms_max);
+            }
+            if (form.squarefeet_max > 0)
+            {
+                query = query.Where(l => l.SquareFeet <= form.squarefeet_max);
+            }
+            return query;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BestDarnRealtorApp/Controllers/ListingController.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestDarnRealtorApp.Data;
+using BestDarnRealtorApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace BestDarnRealtorApp.Controllers
+{
+    [Authorize]
+    public class ListingController : Controller
+    {
+        private readonly BestDarnRealtorAppContext _context;
+
+        public ListingController(BestDarnRealtorAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Listing
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Listing.ToListAsync());
+        }
+
+        // GET: Listing/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listing
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // GET: Listing/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Listing/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Mls,Street1,Street2,City,State,Zip,Neighborhood,SalesPrice,DateListed,Bedrooms,Bathrooms,GarageSize,SquareFeet,LotSize,Description,Image1,Image2,Image3,Image4")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(listing);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listing/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listing.FindAsync(id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            return View(listing);
+        }
+
+        // POST: Listing/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Mls,Street1,Street2,City,State,Zip,Neighborhood,SalesPrice,DateListed,Bedrooms,Bathrooms,GarageSize,SquareFeet,LotSize,Description,Image1,Image2,Image3,Image4")] Listing listing)
+        {
+            if (id != listing.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(listing);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListingExists(listing.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listing/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listing
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // POST: Listing/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var listing = await _context.Listing.FindAsync(id);
+            _context.Listing.Remove(listing);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listing.Any(e => e.Id == id);
+        }
+    }
+}
