9c20ca56bc77cb6017fe02367e6427dd7b6be298
larry <caneslarry@gmail.com>
2021-02-25 17:40:33 -0500

    - inital

---
+++ BestDarnRealtorApp/Data/BestDarnRealtorAppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BestDarnRealtorApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestDarnRealtorApp.Data
+{
+    public class BestDarnRealtorAppContext : DbContext
+    {
+        public BestDarnRealtorAppContext(DbContextOptions<BestDarnRealtorAppContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Listing> Listing { get; set; }
+        public DbSet<User> User { get; set; }
+
+    }
+}
